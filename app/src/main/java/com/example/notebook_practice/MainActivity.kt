c3c626931b1c0e694b894028b0329f6a713815ef
studentDG <daegu776@gmail.com>
2021-10-27 20:02:18 +0900

    로그 찍어 보기

--- app/src/main/java/com/example/notebook_practice/MainActivity.kt
+++ app/src/main/java/com/example/notebook_practice/MainActivity.kt
@@ -2,15 +2,22 @@ package com.example.notebook_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clickBtn
-        smallBtn
+        clickBtn.setOnClickListener {
+            //주석 남기는 법
+//             ctrl + / 커서가 있는 줄을 주석으로 남기는 방법
+//            이{ }안 에 있는 코드만, 버튼이 눌렀을때 실행됨
+            Log.d("메인화면로그","버튼 눌림")
+
+        }
     }
 
 }
